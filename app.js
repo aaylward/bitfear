cc7e2019c756ca04c1cf571c06e2863f383f7cd2
Andy Aylward <aaylward@gmail.com>
2021-08-11 22:09:41 -0400

    paddddd

--- app.js
+++ app.js
@@ -2,8 +2,12 @@
   const area = document.getElementById("source-area");
   const output = document.getElementById("target-area");
 
+  function pad(str) {
+    return "0".repeat(8 - str.length) + str;
+  }
+
   function transform(txt) {
-    return [...txt].map((x) => x.charCodeAt().toString(2)).join(" ")
+    return [...txt].map((x) => pad(x.charCodeAt().toString(2))).join(" ")
   }
 
   area.addEventListener("keyup", (e) => {
